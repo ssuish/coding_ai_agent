f798e88c27be723ad1d376dd76c18731de78bdb9
ssuish <quidor.adrean@gmail.com>
2026-02-15 13:54:01 +0800

    Enhance user interaction by adding command-line argument parsing for dynamic content generation based on user input.

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument("user_prompt", type=str, help="User prompt")
+args = parser.parse_args()
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
 model = "gemini-2.5-flash"
-contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+contents = args.user_prompt
 response = client.models.generate_content(model=model, contents=contents)
 
 print("User prompt:", contents)
